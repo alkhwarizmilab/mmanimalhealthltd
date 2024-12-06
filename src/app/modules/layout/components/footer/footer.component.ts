59e177c2877de122eaf2ce10046836ca7412cbd7
Bodrul Amin <bodrulaminiu@gmail.com>
2024-12-07 00:43:23 +0600

    404

--- src/app/modules/layout/components/footer/footer.component.ts
+++ src/app/modules/layout/components/footer/footer.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {MenubarModule} from "primeng/menubar";
+import {RouterLink} from "@angular/router";
+import {ToolbarModule} from "primeng/toolbar";
+import {Button} from "primeng/button";
+import {CardModule} from "primeng/card";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule,
+    RouterLink,
+    ToolbarModule,
+    Button,
+    CardModule,
+    NgClass
+  ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
+  linkClass = "text-700 hover:text-primary cursor-pointer transition-colors transition-duration-500 p-1";
 
 }
