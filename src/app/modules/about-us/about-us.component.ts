a50ae022352eb9322411bdc3455708f86308851f
Bodrul Amin <bodrulaminiu@gmail.com>
2024-12-09 23:56:22 +0600

    update logo added contact us about us page

--- src/app/modules/about-us/about-us.component.ts
+++ src/app/modules/about-us/about-us.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {CardModule} from "primeng/card";
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule
+  ],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {CardModule} from "primeng/card";
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    Button
+  ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
